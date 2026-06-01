605385cf1e9c209720a8c2c805d268925fbcc443
WittlyEli <162608185+EliksonRod@users.noreply.github.com>
2026-06-01 02:12:49 -0400

    Question 2 commit

--- Assets/Scripts/Mechanics/Health.cs
+++ Assets/Scripts/Mechanics/Health.cs
@@ -21,7 +21,18 @@ namespace Platformer.Mechanics
         /// </summary>
         public bool IsAlive => currentHP > 0;
 
+        /// <summary>
+        /// How long player stays invincible after taking damage.
+        /// </summary>
+        public float InvincibilityDuration = 1f;
+
+        /// <summary>
+        /// Used to manage player invincibility state.
+        /// </summary>
+        bool CanTakeDamage = true;
+
         int currentHP;
+        
 
         /// <summary>
         /// Increment the HP of the entity.
@@ -38,17 +49,7 @@ namespace Platformer.Mechanics
         /// </summary>
         public void Decrement(int dmg)
         {
-            Debug.Log("Damage Received: " + dmg);
-            Debug.Log("Health Before Hit: " + currentHP);
-            currentHP = Mathf.Clamp(currentHP, 0, maxHP);
-            currentHP -= dmg;
-            if (currentHP <= 0)
-            {
-                Debug.Log("Health reached 0, firing HealthIsZero event.");
-                var ev = Schedule<HealthIsZero>();
-                ev.health = this;
-            }
-            Debug.Log("Health After Hit: " + currentHP);
+            StartCoroutine(InvinicibleOnCollision(dmg));
         }
 
         /// <summary>
@@ -63,6 +64,26 @@ namespace Platformer.Mechanics
         {
             currentHP = maxHP;
         }
+        public IEnumerator InvinicibleOnCollision(int dmg)
+        {
+            if (!CanTakeDamage) yield break;
+            CanTakeDamage = false;
 
+            Debug.Log("Damage Received: " + dmg);
+            Debug.Log("Health Before Hit: " + currentHP);
+            currentHP = Mathf.Clamp(currentHP, 0, maxHP);
+            currentHP -= dmg;
+            if (currentHP <= 0)
+            {
+                Debug.Log("Health reached 0, firing HealthIsZero event.");
+                var ev = Schedule<HealthIsZero>();
+                ev.health = this;
+            }
+            Debug.Log("Health After Hit: " + currentHP);
+
+            
+            yield return new WaitForSeconds(InvincibilityDuration);
+            CanTakeDamage = true;
+        }
     }
 }
